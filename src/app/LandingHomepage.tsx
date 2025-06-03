7b2ea74a6d96b32ff9d63f87092c79edf5289e38
Riccardo Giorato <riccardo.giorato@clipboardhealth.com>
2025-06-03 13:09:44 +0200

    feat(branding): add Together.ai logo to LandingHomepage and update footnote styling
    - Introduce Together.ai logo in the LandingHomepage for branding consistency
    - Enhance footnote text with bold styling for DeepSeek R1 and DeepSeek V3

--- src/app/LandingHomepage.tsx
+++ src/app/LandingHomepage.tsx
@@ -10,6 +10,17 @@ export const LandingHomepage = () => {
   return (
     <div className="flex flex-col size-full items-center h-screen justify-center">
       <div className="px-4 md:px-0 pb-4 pt-8 flex flex-col items-center w-full max-w-3xl justify-center gap-4">
+        <div className="w-[180px] relative items-center justify-center rounded bg-gray-50 border border-gray-200 flex flex-row gap-1 px-4 py-2">
+          <div className="text-xs text-[#6A7282] whitespace-nowrap">
+            Powered by
+          </div>
+          <img
+            src="/together.png"
+            className="min-w-[77.3px] h-fit mt-0.5"
+            alt="Together.ai logo"
+          />
+        </div>
+
         <div className="flex flex-col gap-4 w-full mb-14">
           <p className="text-[32px] font-medium text-center text-[#1e2939]">
             Reports with DeepSeek
--- src/components/footnote.tsx
+++ src/components/footnote.tsx
@@ -1,7 +1,8 @@
 export function Footnote() {
   return (
     <div className="text-xs text-zinc-400 leading-5 mx-auto z-[-1]">
-      Powered By DeepSeek R1 & DeepSeek V3 on Together AI
+      Powered By <span className="font-semibold">DeepSeek R1</span> &{" "}
+      <span className="font-semibold">DeepSeek V3</span> on Together AI
     </div>
   );
 }
