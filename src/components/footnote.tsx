e12b70e4c0c266a82847bdd48b345ac7ce63eadc
Riccardo Giorato <riccardo.giorato@clipboardhealth.com>
2025-06-05 15:33:22 +0200

    wip

--- src/components/footnote.tsx
+++
@@ -1,8 +0,0 @@
-export function Footnote() {
-  return (
-    <div className="text-xs text-zinc-400 leading-5 mx-auto z-[-1]">
-      Powered By <span className="font-semibold">DeepSeek R1</span> &{" "}
-      <span className="font-semibold">DeepSeek V3</span> on Together AI
-    </div>
-  );
-}
