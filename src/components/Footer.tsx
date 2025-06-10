63e42ab63842dade9dc81ef4ddeed45601009e50
Riccardo Giorato <riccardo.giorato@clipboardhealth.com>
2025-06-10 13:29:31 +0200

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 export const Footer = () => {
   return (
-    <div className="text-xs text-zinc-400 leading-5 mx-auto z-[-1]">
+    <div className="text-xs text-zinc-400 leading-5 mx-auto">
       Powered By <span className="font-semibold">DeepSeek R1</span> &{" "}
       <span className="font-semibold">DeepSeek V3</span> on Together AI
     </div>
