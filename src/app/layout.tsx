b2fd1db83009878cb32a43d10269d4ce96e7711e
Hassan El Mghari <hassan4709@gmail.com>
2025-06-13 13:23:47 -0400

    added plausible provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,23 @@
-import type { Metadata } from "next";
-import { Figtree } from "next/font/google";
-import "./globals.css";
-import { ClerkProvider } from "@clerk/nextjs";
-import { Toaster } from "sonner";
-import { SidebarProvider } from "@/components/ui/sidebar";
-import { AppSidebar } from "@/components/app/AppSidebar";
-import { Header } from "@/components/Header";
+import type { Metadata } from 'next';
+import { Figtree } from 'next/font/google';
+import './globals.css';
+import { ClerkProvider } from '@clerk/nextjs';
+import { Toaster } from 'sonner';
+import { SidebarProvider } from '@/components/ui/sidebar';
+import { AppSidebar } from '@/components/app/AppSidebar';
+import { Header } from '@/components/Header';
+import { PlausibleProvider } from 'next-plausible';
 
 const figtree = Figtree({
-  variable: "--font-figtree",
-  subsets: ["latin"],
+  variable: '--font-figtree',
+  subsets: ['latin'],
 });
 
 export const metadata: Metadata = {
-  title: "DeepSeek Research",
-  description: "Let AI do research for you",
+  title: 'DeepSeek Research',
+  description: 'Let AI do research for you',
   openGraph: {
-    images: ["https://deepresearch-app.vercel.app/og.jpg"],
+    images: ['https://deepresearch-app.vercel.app/og.jpg'],
   },
 };
 
@@ -30,12 +31,15 @@ export default function RootLayout({
       <SidebarProvider>
         <AppSidebar />
 
-        <html lang="en" className="h-full">
+        <html lang='en' className='h-full'>
+          <head>
+            <PlausibleProvider domain='deepseekresearch.io' />
+          </head>
           <body
             className={`${figtree.variable} flex min-h-full flex-col antialiased`}
           >
             <Header />
-            <Toaster position="top-center" />
+            <Toaster position='top-center' />
             {children}
           </body>
         </html>
